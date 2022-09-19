e93d1d2d17db9c784028a9df48a1ded6a613c36a
Symons-Webb <000812836@mohawkcollege.ca>
2022-09-19 16:49:16 -0400

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  float iVal;
+  int aVal;
+  float resolution;
+
+  resolution = 50/(float)1024;
+  aVal = analogRead(A0);
+  iVal = resolution*aVal; //map(aVal, 0, 1023, 0, 50);
+
+  Serial.print("Digitized output of " + String(aVal) + " is equivalent to a temperature input of " + String(iVal) + " deg. C, which is ");
+
+  if (iVal < 10){
+    Serial.println("Cold!");
+  }else if (iVal > 10 && iVal <= 15){
+    Serial.println("Cool");
+  }else if (iVal > 15 && iVal <= 25 ){
+    Serial.println("Perfect");
+  }else if (iVal > 25 && iVal <= 30 ){
+    Serial.println("Warm");
+  }else if (iVal > 30 && iVal <= 35 ){
+    Serial.println("Hot");
+  }else if (iVal > 35){
+    Serial.println("Too Hot!");
+  }
+
+  //Serial.println("Digitized Value = " + String(iVal));
+
+  delay(500);
+}
